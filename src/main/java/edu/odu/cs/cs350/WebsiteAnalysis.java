4c1d27178b737eaf28f2fc671da6f67c67d9a971
jmora <jmora005@odu.edu>
2019-07-13 18:54:24 -0600

    Start of basic website analysis class to handle user input

---
+++ src/main/java/edu/odu/cs/cs350/WebsiteAnalysis.java
@@ -0,0 +1,13 @@
+package edu.odu.cs.cs350;
+
+public class WebsiteAnalysis 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to WesbiteAnalysis.");
+		System.out.println("Please specify the path to the local copy of the site you want to Analyze.");
+		System.out.println("Please specify the URLs you want to analyze, pressing 'ENTER' between each one.");
+		System.out.println("When you are done, type 'done' and press 'ENTER'. ");
+	}
+}
+ 
