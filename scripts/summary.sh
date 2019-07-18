50bd6bca8a51711e959c2be73ce8a4109a46283d
jmiller <jmiller@cs.odu.edu>
2019-07-18 14:22:27 -0400

    I did user story 1069 please test and verify

---
+++ scripts/summary.sh
@@ -0,0 +1,21 @@
+//Userstory #1069
+
+function getScriptAsText(){
+	var div = document.createElement('div');
+	var scripts = [];
+	var scriptNodes = docmunet.getElementsByTagName('script');
+	
+	for (var i=0, iLen=scriptNodes.length; i<iLen; i++){
+	div.appendChild(scriptNodes[i].cloneNode(true));
+	scripts.push(div.innerHTML);
+	div.removeChild(div.firstChild);
+}
+
+return scripts;
+
+
+
+}
+
+
+
