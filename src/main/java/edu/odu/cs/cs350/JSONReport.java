686f8e860b65ef15378260ec456648e123daf556
ratwell <ratwell@cs.odu.edu>
2019-07-24 01:23:12 +0000

    Fixed bugs/style issues with JSONReport class.

--- src/main/java/edu/odu/cs/cs350/JSONReport.java
+++ src/main/java/edu/odu/cs/cs350/JSONReport.java
@@ -3,83 +3,117 @@ package edu.odu.cs.cs350;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.cedarsoftware.util.io.JsonWriter;
 
 public class JSONReport {
-	
+
+	/**
+	 * 		The name of the JSON file that will be created
+	 */
+
 	private String fileName;
+	/**
+	 * 		The formatted time that the analysis began
+	 */
+
 	private String analysisTime;
+	/**
+	 * 		The formatted JSON string created from the website object
+	 */
+
 	private String json;
+	/**
+	 * 		The file output stream that will write the formatted JSON string
+	 * 		to the file.
+	 */
+
 	private FileOutputStream jsonFile;
-	
+
+	/**
+	 * 		The website object
+	 */
+
 	private Website website;
-	
+
+
+	/**
+	 * @param website
+	 * @param analysisTime
+	 *
+	 * JSONReport takes a website object and the time the analysis started
+	 * to created a formatted JSON file
+	 *
+	 */
 	public JSONReport(Website website, String analysisTime){
-		
+
 		this.setWebsite(website);
 		this.setAnalysisTime(analysisTime);
-		
+
 		setFileName(analysisTime);
-		
+
 	}
-	
+
 	/**
-	 * @param Takes the website object.
-	 * @return The JSON created from the objects of the website as a Formatted String 
+	 * @param website The website object that will be used in the JSON file
+	 * @return json The JSON created from the objects of the website as a Formatted String
 	 */
 	public String writeJSON(Website website) {
-		
+
 		json = JsonWriter.objectToJson(website);
-		
+
 		Map<String, Object> jsonArgs = new HashMap<String, Object>();
-		
+
 		jsonArgs.put(JsonWriter.TYPE, false);
-		jsonArgs.put(JsonWriter.PRETTY_PRINT, true);		
-		
-		json = JsonWriter.objectToJson(website, jsonArgs);		
-		
+		jsonArgs.put(JsonWriter.PRETTY_PRINT, true);
+
+		json = JsonWriter.objectToJson(website, jsonArgs);
+
 		return json;
 	}
-	
+
 	/**
 	 * Creates a JSON file with the jsonFile name
 	 * whose contents are the formatted JSON string
 	 * from the website object.
-	 * 
+	 *
 	 * @throws Exception if fileName already exists
 	 */
 	public void createJSONFile() throws Exception, IOException, FileNotFoundException  {
-				
+
 		//TODO implement try & catch sections for exceptions
 		jsonFile = new FileOutputStream(fileName, true);
-		jsonFile.write(json.getBytes());
+		jsonFile.write(json.getBytes(Charset.forName("UTF-8")));
 		jsonFile.close();
-		
+
 	}
-		
+
 	/**
-	 * @param Takes the analysisTime and appends .json to the end.
+	 * @param analysisTime The formatted time that the analysis starts
+	 *
+	 *  Takes the analysisTime and appends .json to the end.
 	 */
 	public void setFileName(String analysisTime) {
-		this.fileName = analysisTime + ".json"; 
+		this.fileName = analysisTime + ".json";
 	}
-	
+
 	/**
 	 * @return the file name, the formatted analysis time appended with .json
 	 */
 	public String getFileName() {
 		return fileName;
 	}
-	
-	/* 
+
+	/*
 	 * @return the formatted json String
 	 */
+	@Override
 	public String toString() {
 		return json;
-		
+
 	}
 
 	/**
@@ -90,7 +124,8 @@ public class JSONReport {
 	}
 
 	/**
-	 * @param set website object
+	 * @param website
+	 *
 	 */
 	public void setWebsite(Website website) {
 		this.website = website;
@@ -104,12 +139,12 @@ public class JSONReport {
 	}
 
 	/**
-	 * @param sets the analysisTime string
+	 * @param analysisTime
 	 */
 	public void setAnalysisTime(String analysisTime) {
 		this.analysisTime = analysisTime;
 	}
-	
-	
+
+
 
 }
