e295db9efdc991eb8013d2143410d72cbf24a9bf
ratwell <ratwell@cs.odu.edu>
2019-07-25 02:35:32 +0000

    Modified TXTReport & XLSXReport classes to eliminate compilation errors.

--- src/main/java/edu/odu/cs/cs350/TXTReport.java
+++ src/main/java/edu/odu/cs/cs350/TXTReport.java
@@ -3,86 +3,83 @@ package edu.odu.cs.cs350;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
 
 /*
  * still working on this but let me know if it should not be similar to JSONReport class
  * and will redo
- * 
+ *
  */
 
 public class TXTReport {
-	
-// name of txt file
+
+	// name of txt file
 	private String fname;
-// time report was generated (for file name)
+	// time report was generated (for file name)
 	private String analysisTime;
 	private String txt;
 	private FileOutputStream txtFile;
 	private Website website;
-	
+
 	public TXTReport(Website website, String analysisTime) {
-		
+
 		this.setWebsite(website);
 		this.setAnalysisTime(analysisTime);
 		setFname(analysisTime);
-		
+
 	}
-	
+
 	public String writeTXT(Website website) {
-		
-		txt = TxtWriter.objectToTxt(website);
-		
+
+		//TODO txt = TxtWriter.objectToTxt(website);
+
 		//TODO sort pages lexicographically
-		
+
 		return txt;
-		
+
 	}
-	
+
 	public void createTxtFile() throws Exception, IOException, FileNotFoundException {
-		
-		
-		
+
+
+
 	}
-	
-	public void setFname() {
-		
+
+	public void setFname(String analysisTime) {
+
 		this.fname = analysisTime + ".txt";
-		
+
 	}
-	
-	public void getFname() {
-		
+
+	public String getFname() {
+
 		return fname;
-		
+
 	}
-	
+
 	@Override
 	public String toString() {
-		
+
 		return txt;
 	}
-	
+
 	public Website getWebsite() {
-		
+
 		return website;
 	}
-	
+
 	public void setWebsite(Website website) {
-		
+
 		this.website = website;
 	}
-	
+
 	public String getAnalysisTime() {
-		
+
 		return analysisTime;
 	}
-	
-	public void setAnalysisTime() {
-		
+
+	public void setAnalysisTime(String analysisTime) {
+
 		this.analysisTime = analysisTime;
 	}
-	
+
 }
