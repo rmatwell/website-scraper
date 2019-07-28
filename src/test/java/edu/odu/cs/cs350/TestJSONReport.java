cc947f8b9c83a4a8ce3edd9bd7130b2efb9fc4ad
hbrow <hbrow006@odu.edu>
2019-07-28 18:59:33 -0400

    started test class for JSONReport (still working on)

---
+++ src/test/java/edu/odu/cs/cs350/TestJSONReport.java
@@ -0,0 +1,27 @@
+package edu.odu.cs.cs350;
+
+
+public class TestJSONReport {
+	
+	JSONReport defaultConstruct;
+	JSONReport testConstructor;
+	
+	private String analysisTime = "20190725-103257-summary.json";
+	
+	@Before
+	public void setUp() {
+		
+		defaultConstruct = new JSONReport();
+		testConstructor = new JSONReport()
+		
+	}
+	
+	@Test
+	public void testJSONReport() {
+		
+		assertThat(defaultConstruct.getFileName(), is(""));
+		//still working on, will continue tonight
+		
+	}
+	
+}
