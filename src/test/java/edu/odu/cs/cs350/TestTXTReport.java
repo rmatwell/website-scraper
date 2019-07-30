c6c37edfbd3157bc0006d5a25e0e2e7f45efa165
hbrow <hbrow006@odu.edu>
2019-07-29 21:58:48 -0400

    started on test class for TXTReport

---
+++ src/test/java/edu/odu/cs/cs350/TestTXTReport.java
@@ -0,0 +1,38 @@
+package edu.odu.cs.cs350;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTXTReport {
+	
+	TXTReport testTXT, testDefault;
+	
+	private String analysisTime = "20190724-113553-summary"
+	private Website website;
+	
+	@Before setUp() {
+		
+		testDefault = new TXTReport();
+		testTXT = new TXTReport(website, analysisTime);
+		
+	}
+	
+	@Test
+	public void testResource() {
+		
+		assertThat(testDefault.getAnalysisTime(), is(""));
+		assertThat(testDefault.getFileName(), is(""));
+		assertThat(testDefault.toString(), containsString(""));
+		assertThat(testDefault, equalTo(testDefault));
+		
+	}
+	
