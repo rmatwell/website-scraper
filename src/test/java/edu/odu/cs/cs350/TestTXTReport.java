29f9ace677012775174578ea34272e8a8db2521a
hbrow <hbrow006@odu.edu>
2019-07-29 23:00:26 -0400

    forgot to push added test cases to TestTXTReport

--- src/test/java/edu/odu/cs/cs350/TestTXTReport.java
+++ src/test/java/edu/odu/cs/cs350/TestTXTReport.java
@@ -37,5 +37,49 @@ public class TestTXTReport {
 		
 	}
 	
+	@Test
+	public void testTXTReport() {
+		
+		assertThat(testTXT.getAnalysisTime(), is("20190724-113553-summary"));
+
+		testTXT.setFileName(analysisTime);
+		assertThat(testTXT.getFileName(), is("20190724-113553-summary.txt"));
+		assertThat(testTXT.toString(), containsString(""));
+		assertThat(testTXT.getWebsite(), is(website));
+		assertThat(testTXT,  instanceOf(TXTReport.class));
+		assertThat(analysisTime, not(instanceOf(TXTReport.class)));
+		assertNotEquals(testTXT, testDefault);
+		
+	}
+	
+	@Test
+	public void testSetFileName() {
+		
+		int oldHashCode = testTXT.hashCode();
+
+		testTXT.setFileName("20190724-113553-summary");
+
+		assertEquals("20190724-113553-summary.txt", testTXT.getFileName());
+
+		assertNotEquals(oldHashCode, testTXT.hashCode());
+		
+	}
+	
+	@Test
+	public void testClone() throws CloneNotSupportedException {
+
+		testTXT.setFileName(analysisTime);
+		TXTReport aCopy = testTXT.clone();
+
+		assertThat(aCopy.getAnalysisTime(), equalTo(testTXT.getAnalysisTime()));
+		assertThat(aCopy.getFileName(), equalTo(testTXT.getFileName()));
+		assertThat(aCopy.getWebsite(), equalTo(testTXT.getWebsite()));
+		assertThat(aCopy.hashCode(), equalTo(testTXT.hashCode()));
+		assertThat(aCopy.toString(), equalTo(testTXT.toString()));
+		assertThat(aCopy, equalTo(testTXT));
+		
+	}
+}
+	
 }
 	
