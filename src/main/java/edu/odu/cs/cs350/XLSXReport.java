b5d88b82712f13d8a1164c1ba5e5a9a8b4010395
hbrow <hbrow006@odu.edu>
2019-08-03 14:16:20 -0400

    added documentation for XLSXReport class

--- src/main/java/edu/odu/cs/cs350/XLSXReport.java
+++ src/main/java/edu/odu/cs/cs350/XLSXReport.java
@@ -1,7 +1,14 @@
 package edu.odu.cs.cs350;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
+/**
+ * Excel (XLSX) file generator from a Website object to create a single sheet
+ * summary consisting of headers and the corresponding counts
+ * resulting from the analysis.
+ */
 
 public class XLSXReport implements Cloneable {
 
@@ -13,7 +20,7 @@ public class XLSXReport implements Cloneable {
 
 
 	/**
-	 * 
+	 * Empty XLSX report
 	 */
 	public XLSXReport() {
 
@@ -25,6 +32,8 @@ public class XLSXReport implements Cloneable {
 
 
 	/**
+	 * Creates an XLSX file using the website object and 
+	 * time analysis began. 
 	 * @param website
 	 * @param analysisTime
 	 */
@@ -36,56 +45,95 @@ public class XLSXReport implements Cloneable {
 
 	}
 
+	/** 
+	 * Creates formatted XLSX string from analyzed HTML content
+	 * @param website The object that will be used in the XLSX file
+	 * @return xlsx The XLSX file as a formatted string
+	 */
 	public String writeXLSX(Website website) {
 
 		//TODO
 
-		return "";
+		return xlsx;
 
 	}
 
-	public void createXLSXFile() {
+    /**
+     * Creates a XLSX file containing the formatted
+     * XLSX string from website.
+     *
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+	public void createXLSXFile() throws IOException, FileNotFoundException {
 
 		//TODO
 
 	}
 
+    /**
+     * Sets the file name and adds .xlsx file extension.
+     * @param analysisTime
+     *
+     */
 	public void setFileName(String analysisTime) {
 
 		filename = analysisTime + ".xlsx";
 	}
 
+	/**
+     * @return filename as analysis time with .xlsx
+     */
 	public String getFileName() {
 
 		return filename;
 	}
 
+	/**
+     * @return the formatted xlsx string
+     */
 	@Override
 	public String toString() {
 
 		return xlsx;
 	}
 
+	/**
+     * @return the website object
+     */
 	public Website getWebsite() {
 
 		return website;
 	}
 
+	/**
+     * @param website
+     *
+     */
 	public void setWebsite(Website website) {
 
 		this.website = website;
 	}
 
+	/**
+     * @return the analysisTime string
+     */
 	public String getAnalysisTime() {
 
 		return analysisTime;
 	}
 
+    /**
+     * @param analysisTime
+     */
 	public void setAnalysisTime(String analysisTime) {
 
 		this.analysisTime = analysisTime;
 	}
-	
+
+	/**
+	 * Creates a copy of the XLSX object.
+	 */
 	@Override
 	public XLSXReport clone() throws CloneNotSupportedException{
 
