7c67b071f5c3e39c4b58db277ef63c8972f3b719
hbrow <hbrow006@odu.edu>
2019-08-05 23:04:00 -0400

    error fix

--- src/main/java/edu/odu/cs/cs350/HTMLFile.java
+++ src/main/java/edu/odu/cs/cs350/HTMLFile.java
@@ -25,7 +25,7 @@ public class HTMLFile
 		setPageURL(inputURL);
 	}
 	
-	public void getLocalCopyPath() {
+	public File getLocalCopyPath() {
 		
 		return localCopyPath;
 	}
@@ -35,7 +35,7 @@ public class HTMLFile
 		localCopyPath = inputPath;
 	}
 	
-	public void getPageURL() {
+	public URL getPageURL() {
 		
 		return pageURL;
 	}
