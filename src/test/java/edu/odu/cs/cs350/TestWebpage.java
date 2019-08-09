fde0926051cef549b81baeb308ba0a09eb750f7c
ratwell <ratwell@cs.odu.edu>
2019-08-09 03:10:40 +0000

    Updated test coverage for Webpage class.

--- src/test/java/edu/odu/cs/cs350/TestWebpage.java
+++ src/test/java/edu/odu/cs/cs350/TestWebpage.java
@@ -1,6 +1,8 @@
 package edu.odu.cs.cs350;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.HashMap;
@@ -8,10 +10,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.Test;
 import org.junit.Before;
-
-import static org.hamcrest.Matchers.*;
+import org.junit.Test;
 
 /**
  * Test case for Webpage class
@@ -19,10 +19,10 @@ import static org.hamcrest.Matchers.*;
  */
 
 public class TestWebpage {
-	
-	Webpage testWebpage, testDefault;
-	
-	private String path;
+
+    Webpage testWebpage, testDefault;
+
+    private String path = "test/directory/works";
 
     private Map<String, Integer> imageCount = new HashMap<String, Integer>();
     private Map<String, Integer> jsCount = new HashMap<String, Integer>();
@@ -31,13 +31,60 @@ public class TestWebpage {
     private Set<String> scriptPaths = new HashSet<String>();
     private Set<String> cssPaths = new HashSet<String>();
     private Map<String, Integer> linkCount = new HashMap<String, Integer>();
-    
+
+    JSONReport report;
+
     @Before
     public void setUp() {
-    	
-    	testDefault = new Webpage();
-    	testWebpage = new Webpage(path);
-    	
-    }  
-	
+
+        testDefault = new Webpage();
+        testWebpage = new Webpage(path);
+
+    }
+
+    @Test
+    public void testWebpageConstructor() {
+        assertThat(testWebpage.getPath(), is(equalTo(path)));
+
+        testWebpage.setAbsolutePath(path);
+
+        cssCount.put("local", 2);
+        cssCount.put("external", 1);
+        testWebpage.setCssCount(cssCount);
+
+        assertThat(testWebpage.getCssCount(), is(equalTo(cssCount)));
+
+        jsCount.put("local", 2);
+        jsCount.put("external", 1);
+        testWebpage.setJsCount(jsCount);
+
+        assertThat(testWebpage.getjsCount(), is(equalTo(jsCount)));
+
+        imageCount.put("local", 2);
+        imageCount.put("external", 1);
+        testWebpage.setImageCount(imageCount);
+
+        assertThat(testWebpage.getImageCount(), is(equalTo(imageCount)));
+
+        linkCount.put("intra-page", 2);
+        linkCount.put("external", 1);
+        linkCount.put("intra-site", 1);
+        testWebpage.setLinkCount(linkCount);
+
+        assertThat(testWebpage.getLinkCount(), is(equalTo(linkCount)));
+
+        String analysisTime = "20190725-103257-summary";
+
+        report = new JSONReport(testWebpage, analysisTime);
+
+        String json = report.writeJSON(testWebpage);
+
+        assertThat(json, containsString("test/directory/works"));
+
+
+
+
+    }
+
+
 }
